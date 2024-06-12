a90c4307c9746e257847ad104bfd2b2aa8a31a72
tcdhry <hiro2001.ikasamane.0927ya@gmail.com>
2024-06-12 12:08:44 +0900

    エラー処理対応

--- apps/scripts/background/src/main.ts
+++ apps/scripts/background/src/main.ts
@@ -1,22 +1,67 @@
-console.log('chrome-extension-starter - scripts-background');
+import { ChromeGetMyAttendanceInformationResponse, WagoraGetMyAttendanceInformationResponse } from "libs/api";
 
 chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
+    function (request, _sender, sendResponse: (response?: ChromeGetMyAttendanceInformationResponse) => void) {
         fetch('https://g.gxwagora.jp/graphql', {
             method: 'POST',
             body: JSON.stringify({
-                "query": "query getMyAttendanceInformation($from: Date!, $to: Date!) {\n  myAttendanceInformation(from: $from, to: $to) {\n    averageWorkTimeHours\n    userAttendanceInformation {\n      date\n      attendanceTime {\n        begin\n        leaving\n        __typename\n      }\n      signTime {\n        begin\n        leaving\n        signType\n        __typename\n      }\n      retrospectiveComment\n      __typename\n    }\n    __typename\n  }\n}\n",
+                /**
+                 * query getMyAttendanceInformation($from: Date!, $to: Date!) {
+                 *   myAttendanceInformation(from: $from, to: $to) {
+                 *     averageWorkTimeHours
+                 *     userAttendanceInformation {
+                 *       date
+                 *       attendanceTime {
+                 *         begin
+                 *         leaving
+                 *         __typename
+                 *       }
+                 *       signTime {
+                 *         begin
+                 *         leaving
+                 *         signType
+                 *         __typename
+                 *       }
+                 *       retrospectiveComment
+                 *       __typename
+                 *     }
+                 *     __typename
+                 *   }
+                 * }
+                 */
+                query: 'query getMyAttendanceInformation($from: Date!, $to: Date!) {\n  myAttendanceInformation(from: $from, to: $to) {\n    averageWorkTimeHours\n    userAttendanceInformation {\n      date\n      attendanceTime {\n        begin\n        leaving\n        __typename\n      }\n      signTime {\n        begin\n        leaving\n        signType\n        __typename\n      }\n      retrospectiveComment\n      __typename\n    }\n    __typename\n  }\n}\n',
                 // "operationName": "getTeamByKey",
-                "variables": {
+                variables: {
                     from: request.date,
                     to: request.date
                 }
             }),
         }).then(res => {
-            res.json().then((json => {
-                sendResponse(json);
-            }));
-        }).catch(err => { console.log(err); });
+            /**
+             * GraphQLのレスポンス
+             * `status: 200` で errors: Array が存在する場合はエラー
+             * erorrsが存在しなければ成功
+             */
+            res.json().then((json: WagoraGetMyAttendanceInformationResponse) => {
+                if (json.errors) {
+                    sendResponse({
+                        status: 'error',
+                        cause: json.errors,
+                    });
+                } else {
+                    sendResponse({
+                        status: 'success',
+                        data: json.data,
+                    });
+                }
+            });
+        }).catch(err => {
+            console.log({ err });
+            sendResponse({
+                status: 'error',
+                cause: err,
+            });
+        });
         return true;
     }
 );
--- libs/api/src/lib/libs/api.ts
+++ libs/api/src/lib/libs/api.ts
@@ -1,3 +1,135 @@
-export function libsApi(): string {
-  return 'libs/api';
-}
+/**
+ * タブ間通信のレスポンスのベース
+ */
+type InterTabCommunicationResponse<TResponse> = {
+  status: 'success',
+  data: TResponse,
+} | {
+  status: 'error',
+  cause: any,
+};
+
+/**
+ * GraphQLのレスポンスのベース
+ */
+type GraphQLResponse<TResponse> = (
+  {
+    data: null,
+    errors: Array<any>,
+  } | {
+    data: TResponse,
+    errors?: undefined,
+  }
+);
+
+/**
+ * WagoraのGraphQL APIのレスポンス.data部
+ */
+export type GetMyAttendanceInformationResponse = {
+  myAttendanceInformation: {
+    averageWorkTimeHours: number,
+    userAttendanceInformation: Array<{
+      date: string,
+      attendanceTime: Array<{
+        begin: string,
+        leaving: string | null,
+      }>,
+      signTime: Array<{
+        begin: string,
+        leaving: string,
+        signType: string,
+      }>,
+      retrospectiveComment: string,
+    }>,
+  },
+};
+
+export type WagoraGetMyAttendanceInformationResponse = GraphQLResponse<GetMyAttendanceInformationResponse>;
+export type ChromeGetMyAttendanceInformationResponse = InterTabCommunicationResponse<GetMyAttendanceInformationResponse>;
+
+// {
+//     "data": {
+//         "myAttendanceInformation": {
+//             "averageWorkTimeHours": 0,
+//             "userAttendanceInformation": [
+//                 {
+//                     "date": "2024-06-04",
+//                     "attendanceTime": [
+//                         {
+//                             "begin": "2024-06-04T10:04:08.106381+09:00",
+//                             "leaving": null,
+//                             "__typename": "AttendanceTimeModel"
+//                         }
+//                     ],
+//                     "signTime": [
+//                         {
+//                             "begin": "2024-06-04T12:44:24.517602+09:00",
+//                             "leaving": "2024-06-04T13:46:19.679714+09:00",
+//                             "signType": "rest",
+//                             "__typename": "SignTimeModel"
+//                         }
+//                     ],
+//                     "retrospectiveComment": "",
+//                     "__typename": "UserDailyAttendanceInformationModel"
+//                 }
+//             ],
+//             "__typename": "UserWeeklyAttendanceInformationModel"
+//         }
+//     }
+// }
+
+// {
+//   "averageWorkTimeHours": 5.7,
+//   "userAttendanceInformation": [
+//       {
+//           "date": "2024-06-11",
+//           "attendanceTime": [
+//               {
+//                   "begin": "2024-06-11T09:59:14.69908+09:00", // 出勤
+//                   "leaving": "2024-06-11T16:39:06.132624+09:00", // 退勤
+//                   "__typename": "AttendanceTimeModel"
+//               },
+//               {
+//                   "begin": "2024-06-11T16:39:11.039632+09:00", // 出勤
+//                   "leaving": "2024-06-11T16:39:19.088145+09:00", // 退勤
+//                   "__typename": "AttendanceTimeModel"
+//               },
+//               {
+//                   "begin": "2024-06-11T16:39:25.841035+09:00", // 出勤
+//                   "leaving": null, // 未退勤
+//                   "__typename": "AttendanceTimeModel"
+//               }
+//           ],
+//           "signTime": [
+//               {
+//                   "begin": "2024-06-11T13:05:20.632914+09:00",
+//                   "leaving": "2024-06-11T14:07:17.743891+09:00",
+//                   "signType": "rest",
+//                   "__typename": "SignTimeModel"
+//               }
+//           ],
+//           "retrospectiveComment": "",
+//           "__typename": "UserDailyAttendanceInformationModel"
+//       }
+//   ],
+//   "__typename": "UserWeeklyAttendanceInformationModel"
+// }
+
+// {
+//   "status": "success",
+//   "data": {
+//       "myAttendanceInformation": {
+//           "averageWorkTimeHours": 0,
+//           "userAttendanceInformation": [
+//               {
+//                   "date": "2024-06-20",
+//                   "attendanceTime": [],
+//                   "signTime": [],
+//                   "retrospectiveComment": "",
+//                   "__typename": "UserDailyAttendanceInformationModel"
+//               }
+//           ],
+//           "__typename": "UserWeeklyAttendanceInformationModel"
+//       }
+//   }
+// }
